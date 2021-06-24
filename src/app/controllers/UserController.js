c322aaa63c757d9eb552d07732ed12bc0c688e57
lekofox <leandro_dias9@hotmail.com>
2021-06-24 11:51:15 -0300

    adicao de endpoints

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,20 +1,23 @@
 import User from '../models/user';
 import * as Yup from 'yup';
+import { Op, where } from 'sequelize';
 
 class UserController {
   async store(req, res) {
+
     const schema = await Yup.object().shape({
+      id: Yup.number(),
       name: Yup.string().required(),
       lastName: Yup.string().required(),
       nickname: Yup.string().required(),
       address: Yup.string().required(),
 
-      
+
     });
 
     if (!(await schema.isValid(req.body))) {
       return res.status(400).json({
-        message: 'Falha ao cadastrar produto; Por favor corrija os dados para alteração',
+        message: 'Falha ao cadastrar usuário; Por favor corrija os dados para alteração',
       });
     }
 
@@ -26,10 +29,11 @@ class UserController {
       return res.status(400).json({ error: 'Usuário já existe no sistema' });
     }
 
-    const { name, lastName, nickname, address, bio } =
+    const {  name, lastName, nickname, address, bio } =
       await User.create(req.body);
 
     return res.json({
+      
       name,
       lastName,
       nickname,
@@ -38,78 +42,139 @@ class UserController {
     });
   }
 
+  async UserByRef(req, res) {
+    const { name_surname } = req.params;
+    const selectedUser = await User.findAll({
 
-  // async delete(req, res) {
-  //   const { id } = req.params;
+      where: {
 
-  //   const idProduto = await Product.findByPk(id);
-  //   if (!idProduto)
-  //     return res.status(404).json({
-  //       message: 'Erro ao remover; ID selecionado não existe na base',
-  //     });
-  //   await Product.destroy({ where: { id } });
-  //   return res.json({
-  //     message: 'Produto removido com sucesso',
-  //   });
-  // }
+        [Op.or]: [
+          { name: name_surname },
+          { lastName: name_surname }
+        ]
 
-  // async update(req, res) {
-  //   const { id } = req.params;
-  //   const idProduto = await Product.findByPk(id);
+      }
+    });
+    if (selectedUser == '') {
+      return res.status(404).json({ error: 'Nenhum usuário encontrado com esse nome e/ou sobrenome' });
+    }
+    return res.status(200).json([selectedUser]);
+  }
 
-  //   const schema = await Yup.object().shape({
-  //     name: Yup.string().required(),
-  //     descricao: Yup.string().required(),
-  //     preco: Yup.number().required(),
-  //     disponivel: Yup.number().required(),
-  //     destaque: Yup.number().required(),
-  //     estoque: Yup.number().required(),
-  //     id_dep: Yup.number().required()
-  //   });
+  async UserByNickname(req, res) {
+    const { nickname } = req.params;
+
+    const selectedUser = await User.findOne({
+      attributes: ['name', 'lastName', 'nickname'],
+      where: { nickname: nickname }
+
+    })
+
+    if (!selectedUser) {
+      return res.status(404).json({ error: 'Nenhum usuário encontrado com esse apelido' });
+    }
+    return res.status(200).json([selectedUser]);
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+    const idUser = await User.findByPk(id);
+
+    const schema = await Yup.object().shape({
+      lastName: Yup.string().required(),
+      address: Yup.string().required(),
+
+
+    });
+
+    if (!idUser)
+      return res.status(404).json({
+        message: 'Erro ao atualizar usuário; ID selecionado não existe em nossa base',
+      });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        message: 'Falha na validação; Cheque os dados, por favor.',
+      });
+    }
+    else {
+      const { name, lastName, nickname, address, bio } =
+        await idUser.update(req.body);
+
+      return res.json({
+        id,
+        name,
+        lastName,
+        nickname,
+        address,
+        bio,
+      });
+    }
+  }
+
+  async updateNickname(req, res) {
+    const { id } = req.params;
+    const { nickname} = req.body
+    const idUser = await User.findByPk(id);
+    const uniqueNickname = await User.findAndCountAll({
+      where: {nickname:nickname}}
+    )
     
-  //   if (!idProduto)
-  //     return res.status(404).json({
-  //       message: 'Erro ao alterar o produto; ID selecionado não existe em nossa base',
-  //     });
+    
+    const schema = await Yup.object().shape({
+      nickname: Yup.string().required(),
+    });
+    if (!idUser)
+    return res.status(404).json({
+      message: 'Erro ao atualizar usuário; ID selecionado não existe em nossa base',
+    });
 
-  //     if (!(await schema.isValid(req.body))) {
-  //       return res.status(400).json({
-  //         message: 'Falha na validação; Por favor corrija os dados para alteração',
-  //       });
-  //     }
-  //   else {
-  //     const { id, name, descricao, preco, disponivel, destaque,estoque, id_dep } =
-  //       await idProduto.update(req.body);
-  //     return res.json({
-  //       id,
-  //       name,
-  //       preco,
-  //       descricao,
-  //       disponivel,
-  //       estoque,
-  //       destaque,
-  //       id_dep
-  //     });
-  //   }
-  // }
-  // async getAll(req, res) {
-  //   const all = await Product.findAll();
-  //   return res.json(all);
-  // }
+    if (uniqueNickname.count > 0){
+      return res.status(400).json({
+        message: 'Apelido já existe.'
+      })
 
-  // async returnProducts(req, res) {
-  //   const products = await Product.findAll();
-  //   return res.json(products);
-  // }
+    
 
-  // async ProductById(req, res) {
-  //   const { id } = req.params;
-  //   const selectedProduct = await Product.findByPk(id);
-  //   if (!selectedProduct) {
-  //     return res.status(404).json({ error: 'Produto não encontrado' });
-  //   }
-  //   return res.status(200).json({ selectedProduct });
-  // }
+    }else if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        message: 'Falha na validação; Altere seu apelido, por favor.',
+      });
+
+
+      
+    }
+    else {
+      const { name, lastName, nickname, address, bio } =
+        await idUser.update(req.body);
+
+      return res.json({
+
+        name,
+        lastName,
+        nickname,
+        address,
+        bio,
+      });
+    }
+  }
+
+  async delete(req, res) {
+    const { id } = req.params;
+
+    const idUser = await User.findByPk(id);
+    if (!idUser)
+      return res.status(404).json({
+        message: 'Erro ao remover; ID selecionado não existe na base',
+      });
+    await User.destroy({ where: { id } });
+    return res.json({
+      message: 'Usuário removido com sucesso',
+    });
+  }
+
+
+
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -9,38 +9,16 @@ routes.post('/v1/users', UserController.store)
     Se nickname já existe, retornar status e mensagem de erro.
  */
 
+routes.get('/v1/users/:name_surname', UserController.UserByRef)
+
+routes.get('/v1/users/nickname/:nickname', UserController.UserByNickname)
+
+routes.put('/v1/users/:id', UserController.update)
 
 
-routes.get('/v1/users/:name_surname', function (req, res) {
-  /* [] Listar todos os usuários cadastrados filtrados pelos campos nome e/ou sobrenome, 
-  filtrados por parâmetros de consulta: retorna um array de usuários. */
-  
-  res.send('respond with a resource');
-  
-});
+routes.put('/v1/users/nickname/:id', UserController.updateNickname)
 
-routes.get('/v1/users/:nickname', function (req, res, next) {
-  /* [] Listar um usuário pelo nickname passado como parâmetro: retorna um único usuário com nome, sobrenome e nickname. */
-  res.send('respond with a resource');
-});
 
-routes.put('/v1/users/:id', function (req, res, next) {
-  /* [] Alterar o sobrenome e o endereço do usuário recebido no corpo da requisição,
-   baseado no id recebido como parâmetro de rota: retorna o usuário alterado com as novas informações. */
-  res.send('respond with a resource');
-});
-
-routes.put('/v1/users/:id', function (req, res, next) {
-  /* [] Alterar o nickname de um usuário recebido no corpo da requisição, baseado no id recebido como parâmetro de rota: retorna o usuário alterado com as novas informações.
-
-    Se o nickname passado já existir, deve retornar status e mensagem de erro.
- */
-  res.send('respond with a resource');
-});
-
-routes.delete('/v1/users/:id', function (req, res, next) {
-  /* [] Deletar um usuário baseado no id recebido como parâmetro de rota: retorna o status de sucesso */
-  res.send('respond with a resource');
-});
+routes.delete('/v1/users/:id', UserController.delete)
 
 module.exports = routes;
