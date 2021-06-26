300e2674b40fbcaca5c49a86c3d7192b6768cbf1
lekofox <leandro_dias9@hotmail.com>
2021-06-25 22:24:54 -0300

    try...catch add

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -5,202 +5,232 @@ import { v4 as uuidv4 } from 'uuid'
 
 class UserController {
   async store (req, res) {
-    const schema = await Yup.object().shape({
-      id: Yup.string(),
-      name: Yup.string().required(),
-      lastName: Yup.string().required(),
-      nickname: Yup.string().required(),
-      address: Yup.string().required(),
-      bio: Yup.string()
+    try {
+      const schema = await Yup.object().shape({
+        id: Yup.string(),
+        name: Yup.string().required(),
+        lastName: Yup.string().required(),
+        nickname: Yup.string().required(),
+        address: Yup.string().required(),
+        bio: Yup.string()
 
-    })
-
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({
-        message: 'Falha ao cadastrar usuário; Por favor corrija os dados para alteração'
       })
-    }
 
-    const userExist = await User.findOne({
-      where: { nickname: req.body.nickname }
-    })
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({
+          message: 'Falha ao cadastrar usuário; Por favor corrija os dados para alteração'
+        })
+      }
 
-    if (userExist) {
-      return res.status(400).json({ error: 'Usuário já existe no sistema' })
-    }
-
-    const id = uuidv4()
-
-    const { name, lastName, nickname, address, bio } = req.body
-    if (nickname.length > 30) {
-      return res.status(400).json({
-        message: 'O apelido não pode conter mais que 30 caracteres.'
+      const userExist = await User.findOne({
+        where: { nickname: req.body.nickname }
       })
-    } else if (bio !== undefined && bio.length > 100) {
-      return res.status(400).json({
-        message: 'A biografia não pode conter mais que 100 caracteres'
-      })
-    } else {
-      await User.create({ id, name, lastName, nickname, address, bio })
 
-      return res.json({
-        id,
-        name,
-        lastName,
-        nickname,
-        address,
-        bio
-      })
+      if (userExist) {
+        return res.status(400).json({ error: 'Usuário já existe no sistema' })
+      }
+
+      const id = uuidv4()
+
+      const { name, lastName, nickname, address, bio } = req.body
+      if (nickname.length > 30) {
+        return res.status(400).json({
+          message: 'O apelido não pode conter mais que 30 caracteres.'
+        })
+      } else if (bio !== undefined && bio.length > 100) {
+        return res.status(400).json({
+          message: 'A biografia não pode conter mais que 100 caracteres'
+        })
+      } else {
+        await User.create({ id, name, lastName, nickname, address, bio })
+
+        return res.json({
+          id,
+          name,
+          lastName,
+          nickname,
+          address,
+          bio
+        })
+      }
+    } catch (error) {
+      res.status(500)
+      res.json('A sua requisição não foi processada; Por favor tente novamente.')
     }
   }
 
   async userByRef (req, res) {
-    const { nameSurname } = req.params
-    const selectedUsers = await User.findAll({
+    try {
+      const { nameSurname } = req.params
+      const selectedUsers = await User.findAll({
 
-      where: {
+        where: {
 
-        [Op.or]: {
+          [Op.or]: {
 
-          [Op.or]: [
-            { name: nameSurname },
-            { lastName: nameSurname }
+            [Op.or]: [
+              { name: nameSurname },
+              { lastName: nameSurname }
 
-          ],
-          [Op.and]: {
-            query: sequelize.where(
-              sequelize.fn(
-                'concat',
-                sequelize.col('name'),
-                ' ',
-                sequelize.col('lastName')
-              ),
-              {
-                [sequelize.Op.like]: nameSurname
-              }
-            )
+            ],
+            [Op.and]: {
+              query: sequelize.where(
+                sequelize.fn(
+                  'concat',
+                  sequelize.col('name'),
+                  ' ',
+                  sequelize.col('lastName')
+                ),
+                {
+                  [sequelize.Op.like]: nameSurname
+                }
+              )
+
+            }
 
           }
-
         }
-      }
-    })
+      })
 
-    // eslint-disable-next-line eqeqeq
-    if (selectedUsers == '') {
-      return res.status(404).json({ message: 'Nenhum usuário encontrado com esse nome e/ou sobrenome' })
+      // eslint-disable-next-line eqeqeq
+      if (selectedUsers == '') {
+        return res.status(404).json({ message: 'Nenhum usuário encontrado com esse nome e/ou sobrenome' })
+      }
+      return res.status(200).json(selectedUsers)
+    } catch (error) {
+      res.status(500)
+      res.json('A sua requisição não foi processada; Por favor tente novamente.')
     }
-    return res.status(200).json(selectedUsers)
   }
 
   async userByNickname (req, res) {
-    const { nickname } = req.params
+    try {
+      const { nickname } = req.params
 
-    const selectedUser = await User.findOne({
-      attributes: ['name', 'lastName', 'nickname'],
-      where: { nickname: nickname }
+      const selectedUser = await User.findOne({
+        attributes: ['name', 'lastName', 'nickname'],
+        where: { nickname: nickname }
 
-    })
+      })
 
-    if (!selectedUser) {
-      return res.status(404).json({ error: 'Nenhum usuário encontrado com esse apelido' })
+      if (!selectedUser) {
+        return res.status(404).json({ error: 'Nenhum usuário encontrado com esse apelido' })
+      }
+      return res.status(200).json([selectedUser])
+    } catch (error) {
+      res.status(500)
+      res.json('A sua requisição não foi processada; Por favor tente novamente.')
     }
-    return res.status(200).json([selectedUser])
   }
 
   async update (req, res) {
-    const { id } = req.params
-    const idUser = await User.findByPk(id)
+    try {
+      const { id } = req.params
+      const idUser = await User.findByPk(id)
 
-    const schema = await Yup.object().shape({
-      lastName: Yup.string().required(),
-      address: Yup.string().required()
+      const schema = await Yup.object().shape({
+        lastName: Yup.string().required(),
+        address: Yup.string().required()
 
-    })
-
-    if (!idUser) {
-      return res.status(404).json({
-        message: 'Erro ao atualizar usuário; ID selecionado não existe em nossa base'
       })
-    }
 
-    if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({
-        message: 'Falha na validação; Cheque os dados, por favor.'
-      })
-    } else {
-      const { name, lastName, nickname, address, bio } =
+      if (!idUser) {
+        return res.status(404).json({
+          message: 'Erro ao atualizar usuário; ID selecionado não existe em nossa base'
+        })
+      }
+
+      if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({
+          message: 'Falha na validação; Cheque os dados, por favor.'
+        })
+      } else {
+        const { name, lastName, nickname, address, bio } =
         await idUser.update(req.body)
 
-      return res.json({
-        id,
-        name,
-        lastName,
-        nickname,
-        address,
-        bio
-      })
+        return res.json({
+          id,
+          name,
+          lastName,
+          nickname,
+          address,
+          bio
+        })
+      }
+    } catch (error) {
+      res.status(500)
+      res.json('A sua requisição não foi processada; Por favor tente novamente.')
     }
   }
 
   async updateNickname (req, res) {
-    const { id } = req.params
-    const { nickname } = req.body
-    const idUser = await User.findByPk(id)
-    const uniqueNickname = await User.findAndCountAll({
-      where: { nickname: nickname }
-    }
-    )
+    try {
+      const { id } = req.params
+      const { nickname } = req.body
+      const idUser = await User.findByPk(id)
+      const uniqueNickname = await User.findAndCountAll({
+        where: { nickname: nickname }
+      }
+      )
 
-    const schema = await Yup.object().shape({
-      nickname: Yup.string().required()
-    })
-    if (!idUser) {
-      return res.status(404).json({
-        message: 'Erro ao atualizar usuário; ID selecionado não existe em nossa base'
+      const schema = await Yup.object().shape({
+        nickname: Yup.string().required()
       })
-    }
+      if (!idUser) {
+        return res.status(404).json({
+          message: 'Erro ao atualizar usuário; ID selecionado não existe em nossa base'
+        })
+      }
 
-    if (uniqueNickname.count > 0) {
-      return res.status(400).json({
-        message: 'Apelido já existe.'
-      })
-    } else if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({
-        message: 'Falha na validação; Insira seu apelido, por favor.'
-      })
-    } else if (nickname.length > 30) {
-      return res.status(400).json({
-        message: 'O apelido não pode conter mais que 30 caracteres.'
-      })
-    } else {
-      const { name, lastName, nickname, address, bio } =
+      if (uniqueNickname.count > 0) {
+        return res.status(400).json({
+          message: 'Apelido já existe.'
+        })
+      } else if (!(await schema.isValid(req.body))) {
+        return res.status(400).json({
+          message: 'Falha na validação; Insira seu apelido, por favor.'
+        })
+      } else if (nickname.length > 30) {
+        return res.status(400).json({
+          message: 'O apelido não pode conter mais que 30 caracteres.'
+        })
+      } else {
+        const { name, lastName, nickname, address, bio } =
         await idUser.update(req.body)
 
-      return res.json({
+        return res.json({
 
-        name,
-        lastName,
-        nickname,
-        address,
-        bio
-      })
+          name,
+          lastName,
+          nickname,
+          address,
+          bio
+        })
+      }
+    } catch (error) {
+      res.status(500)
+      res.json('A sua requisição não foi processada; Por favor tente novamente.')
     }
   }
 
   async delete (req, res) {
-    const { id } = req.params
+    try {
+      const { id } = req.params
 
-    const idUser = await User.findByPk(id)
-    if (!idUser) {
-      return res.status(404).json({
-        message: 'Erro ao remover; ID selecionado não existe na base'
+      const idUser = await User.findByPk(id)
+      if (!idUser) {
+        return res.status(404).json({
+          message: 'Erro ao remover; ID selecionado não existe na base'
+        })
+      }
+      await User.destroy({ where: { id } })
+      return res.status(200).json({
+        message: 'Usuário removido com sucesso'
       })
+    } catch (error) {
+      res.status(500)
+      res.json('A sua requisição não foi processada; Por favor tente novamente.')
     }
-    await User.destroy({ where: { id } })
-    return res.status(200).json({
-      message: 'Usuário removido com sucesso'
-    })
   }
 }
 
